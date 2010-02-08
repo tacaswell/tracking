90730f7b17de2a0bf8e480ae477f9a979e05d6b0
Thomas A Caswell <tcaswell@uchicago.edu>
2010-02-08 23:58:03 +0000

    added plotting routine for plotting phi6 data as a quiver plot
    git-svn-id: svn://innoue/tracking/trunk@145 4cb6fdf0-3dc9-4801-8ffc-c6659000aa44

---
+++ script_src/python/visualization/plot_phi6.py
@@ -0,0 +1,44 @@
+#Copyright 2010 Thomas A Caswell
+#tcaswell@uchicago.edu
+#http://jfi.uchicago.edu/~tcaswell
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 3 of the License, or (at
+#your option) any later version.
+#
+#This program is distributed in the hope that it will be useful, but
+#WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, see <http://www.gnu.org/licenses>.
+
+
+from numpy import *
+import matplotlib.pyplot as plt
+import random
+import itertools
+import h5py
+
+
+def plot_file_frame(f,fr_num):
+    x = f["/frame%(#)06d"%{"#":fr_num}+"/x"]
+    y = f["/frame%(#)06d"%{"#":fr_num}+"/y"]
+    phi = f["/frame%(#)06d"%{"#":fr_num}+"/scaler_order_parameter"]
+    phir = zeros(phi.shape[0])
+    phii = zeros(phi.shape[0])
+    for j in range(0,phi.shape[0]):
+        phir[j] = phi[j][0]
+        phii[j] = phi[j][1]
+        
+
+    plt.quiver(x,y,phir,phii)
+    plt.plot(x,y,'ro')
+    plt.show()
+
+f = h5py.File("test.h5",'r')
+
+plot_file_frame(f,0)
+f.close()
