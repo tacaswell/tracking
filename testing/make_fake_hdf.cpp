c0d186a312b91b94a7bb4fea9d939aa20ddfa2c6
Thomas A Caswell <tcaswell@uchicago.edu>
2010-02-23 00:27:39 +0000

    changed to deal with new wrapper_o_hdf api
    git-svn-id: svn://innoue/tracking/trunk@166 4cb6fdf0-3dc9-4801-8ffc-c6659000aa44

--- testing/make_fake_hdf.cpp
+++ testing/make_fake_hdf.cpp
@@ -209,7 +209,7 @@ int main(int argc, const char * argv[])
     string fname = "test.hdf";
 
     
-    Wrapper_o_hdf hdf_w(fname,data_types,322,"frame",false,false,false);
+    Wrapper_o_hdf hdf_w(fname,data_types,322,Wrapper_o_hdf::NEW_FILE_OVR, "frame");
 
     
     try
