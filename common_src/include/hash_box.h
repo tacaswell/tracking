9a26113fe5fa6c7c689b7de576dbb8c88a8ded32
Thomas A Caswell <tcaswell@uchicago.edu>
2010-03-30 21:54:21 +0000

    took out final bits of old g(r) code
    added code for Accumulator cascades to hash_box
    
    git-svn-id: svn://innoue/tracking/trunk@224 4cb6fdf0-3dc9-4801-8ffc-c6659000aa44

--- common_src/include/hash_box.h
+++ common_src/include/hash_box.h
@@ -45,6 +45,7 @@ class Wrapper_out;
 namespace tracking{
 
 class Corr;
+class Accumulator;
 class Hash_shelf;
 
 /**
@@ -175,59 +176,6 @@ public:
   ///@}
   
   
-  ///@name g(r) functions
-  ///@{
-  /**Returns the number of particles that were used as centers and
-   * returns the results by reference.  g(r) is computed for the
-   * particles contained in *this against the particles contained in
-   * *points.  The results are returned in two vectors, bin_count is
-   * the number of particles in each bin and bin_r is the radius of
-   * the inner edge of each bin. It takes two arguments, max_d is the
-   * maximum distance to consider, nbins is the number of bins.  This
-   * function uses linearly distributed boxes.  This function only
-   * uses free code.
-   * 
-   * This code assumes that bin_count and bin_r contain junk and need
-   * to be initialized.  Any data in them will be cleared
-
-   * the underlying data structures should be clever enough work out
-   * how many dimensions they are in.
-  */
-  int gofr(float max_d, int nbins, hash_box* points,
-	   std::vector<float>& bin_count,std::vector<float>& bin_r);
-
-
-  ///Same as gofr, but assumes that bin_count is properly initialized
-  int gofr(float max_d, int nbins, hash_box* points,
-	   std::vector<float>& bin_count);
-
-  ///Same as gofr, with the bin count vector is normalized with
-  ///respect to area
-  int gofr_norm_a(float max_d, unsigned int nbins, hash_box* points,
-	   std::vector<float>& bin_count,std::vector<float>& bin_r);
-  
-  ///Same as gofr_norm_a, with the bin count vector is normalized with
-  ///respect to area and the local average density
-  int gofr_norm_an(float max_d, int nbins, 
-		   std::vector<float> & bin_count,std::vector<float>& bin_r);
-
-
-  ///Returns g(r).  The information is returned in two vectors,
-  ///bin_count is the number of particles in each bin and bin_r is the
-  ///radius of the center of each bin. It takes two arguments, max_d
-  ///is the maximum distance to consider, nbins is the number of bins.
-  ///This function uses linearly distributed boxes.  This function
-  ///uses intel code
-  void gofr_ipp(unsigned int max_d, unsigned int nbins, 
-       std::vector<int> bin_count,std::vector<float> bin_r);
-  
-  /**
-     computes 2D histogram 
-   */
-  int gofr2D(float max_d, int nbins, hash_box* points,
-		       std::vector<float>& bin_count);
-    
-  ///@}
 
   /**
      Passes functions down the pyramid, this one for void, argument-less
@@ -251,6 +199,11 @@ public:
      passes a Corr object down the pyramid
    */
   void compute_corr(tracking::Corr &) const ;
+
+  /**
+     Passes an Accumulator object down the pyramid
+   */
+  void compute_accum(Accumulator & in)const;
   
 };
 }
