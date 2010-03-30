9baa8c464014d83c4fe3949aea3b2cfd822ab11f
Thomas A Caswell <tcaswell@uchicago.edu>
2010-03-30 21:54:19 +0000

    New base class for dealing with things that take in a particle, accumulate information about the, but don't care about what is around the particle (ie s(q))
    git-svn-id: svn://innoue/tracking/trunk@219 4cb6fdf0-3dc9-4801-8ffc-c6659000aa44

---
+++ common_src/include/accumulator.h
@@ -0,0 +1,79 @@
+//Copyright 2010 Thomas A Caswell
+//tcaswell@uchicago.edu
+//http://jfi.uchicago.edu/~tcaswell
+//
+//This program is free software; you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation; either version 3 of the License, or (at
+//your option) any later version.
+//
+//This program is distributed in the hope that it will be useful, but
+//WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program; if not, see <http://www.gnu.org/licenses>.
+//
+//Additional permission under GNU GPL version 3 section 7
+//
+//If you modify this Program, or any covered work, by linking or
+//combining it with IPP (or a modified version of that library),
+//containing parts covered by the terms of End User License Agreement
+//for the Intel(R) Software Development Products, the licensors of
+//this Program grant you additional permission to convey the resulting
+//work.
+
+#ifndef CUM_
+#define CUM_
+
+#include <vector>
+
+#include "part_def.h"
+
+namespace utilities
+{
+class Generic_wrapper;
+}
+
+
+
+
+
+namespace tracking
+{
+
+
+
+/**
+   An ABC of objects that can be handed into hash_case and calculates
+   correlation functions. 
+ */
+class Accumulator
+{
+public:
+  /**
+     takes in a particle_base object.  For correlations that need the
+     tracking information this should throw an error or something
+     clever like that
+   */
+  virtual void add_particle(const particle *) = 0;
+
+  /**
+     outputs the result of the computation to the wrapper
+   */
+  virtual void out_to_wrapper(utilities::Generic_wrapper & ) const =0;
+  /**
+     returns the maximum range that the Corr object looks out to.  This is needed so
+     that the Hash_shelf can properly limit the particles that it hands in
+   */
+  virtual ~Accumulator(){}; 
+  
+
+};
+
+
+}
+
+
+#endif
