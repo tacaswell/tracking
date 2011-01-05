542867908ed804ca63bc8122290dfc37a695c94a
Thomas A Caswell <tcaswell@uchicago.edu>
2011-01-05 17:50:16 -0600

    Added sql handling to gofr_hdf
    changed naming conventions of variable to match most other code
    
    changed indentation
    
    added md_store to out_to_wrapper

--- apps/gofr_hdf.cpp
+++ apps/gofr_hdf.cpp
@@ -52,6 +52,10 @@
 #include "read_config.h"
 
 #include "enum_utils.h"
+
+#include "sql_handler.h"
+#include "md_store.h"
+
 //#include "gnuplot_i.hpp" //Gnuplot class handles POSIX-Pipe-communication with Gnuplot
 
 using std::cout;
@@ -64,6 +68,8 @@ using std::logic_error;
 
 using utilities::Wrapper_o_hdf;
 using utilities::Wrapper_i_hdf;
+using utilities::Md_store;
+using utilities::SQL_handler;
 
 using utilities::Tuple;
 using utilities::Filter_basic;
@@ -86,6 +92,9 @@ static string APP_NAME = "gofr :: ";
 
 /**
    \page gofr_doc g(r) HDF Documentation
+
+
+   THIS IS SLIGHTLY OUT OF DATE
 
    This program takes in an hdf file of particle locations, computes 
    \f$g(r)\f$, and writes the results out to an hdf file.
@@ -133,53 +142,39 @@ int main(int argc, char * argv[])
 {
 
   		   
-  string data_file ;
-  string out_file ;
   string pram_file ;
   
   
     
   int optchar;
-  bool found_i=false,found_o= false,found_c = false;
+  bool found_c = false;
     
   while((optchar = getopt(argc,argv,"i:o:c:")) !=-1)
   {
     switch(optchar)
     {
-    case 'i':
-      data_file = string(optarg);
-      found_i = true;
-      break;
-    case 'o':
-      out_file = string(optarg);
-      found_o = true;
-      break;
     case 'c':
       pram_file = string(optarg);
       found_c = true;
       break;
-      
+    case 'i':
+    case 'o':
+    case 'd':
+      break;
     case '?':
     default:
-      cout<<"-i input file name"<<endl;
-      cout<<"-o output file name"<<endl;
       cout<<"-c parameter file name"<<endl;
       break;
     }
   }
 
-  if(!(found_i && found_o && found_c))
+  if(!found_c)
   {
     cerr<<"input failed"<<endl;
-    cout<<"-i input filename"<<endl;
-    cout<<"-o output filename"<<endl;
     cout<<"-c configuration filename"<<endl;
     return -1;
   }
   
-  cout<<"file to read in: "<<data_file<<endl;
-  cout<<"file that will be written to: "<<out_file<<endl;
-
   // parsing of parameters
 
 
@@ -206,17 +201,16 @@ int main(int argc, char * argv[])
     return -1;
   }
   
-  int read_comp_num, write_comp_num,dset_num;
+  int read_comp_num, write_comp_num,dset_key;
   Read_config comp_prams(pram_file,"comps");
-  if(!(comp_prams.contains_key("read_comp")&&
-       comp_prams.contains_key("write_comp")))
+  if(!(comp_prams.contains_key("iden_key")&&
+       comp_prams.contains_key("dset_key")))
     throw logic_error(APP_NAME + 
 		      " parameter file does not contain both read and write comp nums");
   try
   {
-    comp_prams.get_value("read_comp",read_comp_num);
-    comp_prams.get_value("write_comp",write_comp_num);
-    comp_prams.get_value("dset",dset_num);
+    comp_prams.get_value("iden_key",read_comp_num);
+    comp_prams.get_value("dset_key",dset_key);
   }
   catch(logic_error & e)
   {
@@ -225,67 +219,99 @@ int main(int argc, char * argv[])
     return -1;
   }
   
-
-
+  string db_path;
+  string in_file ;
+  string out_file ;
+  Read_config files(pram_file,"files");
   try
   {
-    D_TYPE tmp[] = {utilities::D_XPOS,
-		    utilities::D_YPOS,
-		    utilities::D_DX,
-		    utilities::D_DY,
-		    utilities::D_I,
-		    utilities::D_R2,
-		    utilities::D_E
-		    };
-    set<D_TYPE> data_types = set<D_TYPE>(tmp, tmp+7);
+    files.get_value("db_path",db_path);
+    files.get_value("fin",in_file);
+    files.get_value("fout",out_file);
+  }
+  catch(logic_error & e)
+  {
+    cerr<<"error parsing the file parameters"<<endl;
+    cerr<<e.what()<<endl;
+    return -1;
+  }
+
+  cout<<"file to read in: "<<in_file<<endl;
+  cout<<"file that will be written to: "<<out_file<<endl;
+  
+  
+  // set up sql stuff
+  SQL_handler sql;
+  sql.open_connection(db_path);
+
+
+  // claim the entry in the DB and lock it before doing anything
+
+  sql.start_comp(dset_key,write_comp_num,utilities::F_GOFR);
+
+
+
+  D_TYPE tmp[] = {utilities::D_XPOS,
+		  utilities::D_YPOS,
+		  utilities::D_DX,
+		  utilities::D_DY,
+		  utilities::D_I,
+		  utilities::D_R2,
+		  utilities::D_E
+  };
+  set<D_TYPE> data_types = set<D_TYPE>(tmp, tmp+7);
 
   
     
-    Wrapper_i_hdf wh(data_file,data_types,read_comp_num);
+  Wrapper_i_hdf wh(in_file,data_types,read_comp_num);
 
     
 
-    Master_box box;
-    Filter_basic filt(data_file,read_comp_num);
-    //    Filter_trivial filt;
+  Master_box box;
+  Filter_basic filt(in_file,read_comp_num);
+  Md_store filt_md = filt.get_parameters();
+  //    Filter_trivial filt;
     
 
-    box.init(wh,filt);
+  box.init(wh,filt);
     
 
     
-    cout<<"total number of particles is: "<<box.size()<<endl;;
+  cout<<"total number of particles is: "<<box.size()<<endl;;
     
-    Tuple<float,2> dims = wh.get_dims();
-    hash_case hcase;
-    hcase.init(box,dims,max_range/2,wh.get_num_frames());
+  Tuple<float,2> dims = wh.get_dims();
+  hash_case hcase;
+  hcase.init(box,dims,max_range/2,wh.get_num_frames());
 
-    cout<<"hash case filled"<<endl;
+  cout<<"hash case filled"<<endl;
     
-    Corr_gofr gofr(nbins,max_range,write_comp_num,dset_num,read_comp_num);
-    hcase.compute_corr(gofr);
-    cout<<"computed g(r)"<<endl;
+  Corr_gofr gofr(nbins,max_range,write_comp_num,dset_key,read_comp_num);
+  hcase.compute_corr(gofr);
+  cout<<"computed g(r)"<<endl;
     
-    //    gofr.display();
+  //    gofr.display();
+  
+  
+  // set up md_store
+  Md_store md_store;
+  md_store.add_elements(app_prams.get_store());    
+  md_store.add_elements(comp_prams.get_store());    
+  md_store.add_elements(files.get_store());    
+  md_store.add_elements(&filt_md);
+  md_store.add_element("comp_key",write_comp_num);
 
       
-    Generic_wrapper_hdf hdf_out(out_file,true);
-    gofr.out_to_wrapper(hdf_out,format_name(grp_name,write_comp_num));
-    cout<<"wrote out g(r)"<<endl;
+  Generic_wrapper_hdf hdf_out(out_file,true);
+  gofr.out_to_wrapper(hdf_out,format_name(grp_name,write_comp_num),&md_store);
+  cout<<"wrote out g(r)"<<endl;
 
-    
-  }
-  catch(const char * err){
-    std::cerr<<"caught on error: ";
-    std::cerr<<err<<endl;
-    return -1;
-  } 
-  catch(std::exception & e)
-  {
-    cerr<<"died on exception"<<endl;
-    cerr<<'\t'<<e.what()<<endl;
-    return -1;
-  }
+  // shove in to db
+  sql.add_mdata(md_store);
+  sql.commit();
+  
+  // clean up the data base
+  sql.close_connection();
+
   
   return 0;
 }
