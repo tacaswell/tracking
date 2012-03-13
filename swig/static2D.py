863c52a34077d7d69abf0f09522ea243d3be007e
Thomas A Caswell <tcaswell@uchicago.edu>
2012-03-13 16:36:28 -0500

    first pass at writing skeleton for  friendly interface for python

---
+++ swig/static2D.py
@@ -0,0 +1,33 @@
+#Copyright 2012 Thomas A Caswell
+#tcaswell@uchicago.edu
+#http://jfi.uchicago.edu/~tcaswell
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 3 of the License, or (at
+#your option) any later version.
+#
+#This program is distributed in the hope that it will be useful, but
+#WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, see <http://www.gnu.org/licenses>.
+import cstatic2D as cs
+
+class hash_case:
+	'''Public facing thing that wraps hash_case'''
+	def add_data(self, data):
+		'''Takes in a data and fills the hash_case '''
+		pass
+	def do_corr(self,corr):
+		'''Computes a correlation function, requires a c++ class that is wrapped'''
+	pass
+
+
+class wrapper_i_python:
+	'''public facing wrapper for numpy -> internal structures'''
+	pass
+
+
