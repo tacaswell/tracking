de362def7b660d5df7c9941cce73941a7d860bdc
Thomas A Caswell <tcaswell@gmail.com>
2012-05-06 15:47:19 -0500

    syntax fix

--- common_src/cxx/accum_sofq.cpp
+++ common_src/cxx/accum_sofq.cpp
@@ -93,7 +93,7 @@ void Accum_sofq::add_particle(const particle * p_in)
   
 }
 
-void Accum_sofq::out_to_wrapper(utilities::Generic_wrapper & wrap,,
+void Accum_sofq::out_to_wrapper(utilities::Generic_wrapper & wrap,
                                 const utilities::Md_store & md_store) const 
 {
 
