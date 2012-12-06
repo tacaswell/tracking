08f61d1bb0ccc95183f951e000ce6abcd28338b6
Patrick Figliozzi <pfigliozzi@uchicago.edu>
2012-12-06 16:45:25 -0600

    updated cmakelist.txt and findipp to work on a different computer

--- cmake/Modules/FindIPP.cmake
+++ cmake/Modules/FindIPP.cmake
@@ -28,32 +28,43 @@ SET(IPP_PROCESS_INCLUDES  IPP_PROCESS_INCLUDE)
 
 # Finally the library itself
 find_library(IPPI_LIB 
-  NAMES ippiem64t    
+  NAMES ippi    
   HINTS $ENV{IPP_ROOT}/sharedlib
+HINTS $ENV{IPP_ROOT}/lib/intel64
 )		     
 find_library(IPPS_LIB
-  NAMES ippsem64t    
+  NAMES ipps  
   HINTS $ENV{IPP_ROOT}/sharedlib
+HINTS $ENV{IPP_ROOT}/lib/intel64
 )		     
 find_library(IPPV_LIB
-  NAMES ippvmem64t
+  NAMES ippvm
   HINTS $ENV{IPP_ROOT}/sharedlib
+HINTS $ENV{IPP_ROOT}/lib/intel64
 )
 find_library(IPPC_LIB
-  NAMES ippcoreem64t
+  NAMES ippcore
   HINTS $ENV{IPP_ROOT}/sharedlib
+HINTS $ENV{IPP_ROOT}/lib/intel64
 )
 find_library(IPPCV_LIB
-  NAMES ippcvem64t
+  NAMES ippcv
   HINTS $ENV{IPP_ROOT}/sharedlib
+HINTS $ENV{IPP_ROOT}/lib/intel64
+
 )
 
+find_library(IPPOMP_LIB
+   NAMES iomp5
+   HINTS /opt/intel/composer_xe_2013.1.117/compiler/lib/intel64
+)
 
 message(STATUS "${IPPI_LIB}")
 message(STATUS "${IPPS_LIB}")
 message(STATUS "${IPPV_LIB}")
 message(STATUS "${IPPC_LIB}")
 message(STATUS "${IPPCV_LIB}")
+message(STATUS "${IPPOMP_LIB}")
 # Set the include dir variables and the libraries and let libfind_process do the rest.
 # NOTE: Singular variables for this library, plural for libraries this this lib depends on.
 
@@ -62,6 +73,6 @@ set(IPP_PROCESS_LIBS ${IPP_PROCESS_LIBS} IPPS_LIB)
 set(IPP_PROCESS_LIBS ${IPP_PROCESS_LIBS} IPPV_LIB)
 set(IPP_PROCESS_LIBS ${IPP_PROCESS_LIBS} IPPC_LIB)
 set(IPP_PROCESS_LIBS ${IPP_PROCESS_LIBS} IPPCV_LIB)
-
+set(IPP_PROCESS_LIBS ${IPP_PROCESS_LIBS} IPPOMP_LIB)
 
 libfind_process(IPP)
